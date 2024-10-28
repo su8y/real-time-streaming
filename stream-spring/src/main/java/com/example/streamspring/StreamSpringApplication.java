328ccb3280f3e5642aef4d221e50a6f5c0d30cae
su8y <bsa0530@gmail.com>
2024-10-28 10:52:00 +0900

    Init spring project

---
+++ stream-spring/src/main/java/com/example/streamspring/StreamSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.streamspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamSpringApplication.class, args);
+    }
+
+}
